77174c335a115d4841eff04aed0917b98d79430c
Matylda Czuba <matylda.czuba@pm.me>
2018-10-13 15:26:00 +0200

    6

---
+++ js/6-animacje.js
@@ -0,0 +1,26 @@
+$('p').on('click', function(){
+   
+//    $(this).hide(1500);
+//    $(this).fadeOut(1500);
+    $(this).slideUp(1500);
+
+    
+});
+
+$('h1').on('click', function(){
+    
+//    $('p').fadeIn(1500);
+    $('p').slideDown(1500);
+
+});
+
+
+$('.anim').on('click', function(){
+   $(this).animate(
+   {
+       'width': '600px',
+       'height': '600px',
+       'left': '300px'
+   }
+   );
+});
